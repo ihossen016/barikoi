36fdc5e10bb21759e5f974d2326fd6700c2eb46f
ihossen016 <ihossen016@gmail.com>
2022-03-24 23:29:40 +0600

    Search section created

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Search from "./components/Search";
+import Maps from "./components/Maps";
+
+function App() {
+  return (
+    <div className="App">
+      <Search />
+      <Maps />
+    </div>
+  );
+}
+
+export default App;
